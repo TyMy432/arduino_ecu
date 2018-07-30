dded312fae799d0c22b41e7952c36d0a0b11f73c
TyMy432 <isaac.smirle@gmail.com>
2018-07-30 16:34:06 -0400

    initial commit

---
+++ Arduino Code.cpp
@@ -0,0 +1,18 @@
+
+
+
+// void setup() {
+//   // put your setup code here, to run once:
+  
+
+
+
+//   Scheduler.startLoop(Loop2);     //Loop2 controls running condition sensor inputs and interpretation
+//   Scheduler.startLoop(Loop3);     //Loop3 runs fuel/air/timing algorithems based on Loop2
+//   Scheduler.startLoop(Loop4);     //Loop4 calculates 
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+
+// }
