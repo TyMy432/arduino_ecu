9d3ae84f3cb54a4a8c836f3d71d13fecadd2dd15
TyMy432 <isaac.smirle@gmail.com>
2018-07-31 11:01:13 -0400

    5 loops

--- function scheduler SOURCE.cpp
+++ function scheduler SOURCE.cpp
@@ -9,11 +9,12 @@
 
    Scheduler.startLoop(Loop2);     //Loop2 controls running condition sensor inputs and interpretation
    Scheduler.startLoop(Loop3);     //Loop3 runs fuel/air/timing algorithems based on Loop2
-   Scheduler.startLoop(Loop4);     //Loop4 calculates 
+   Scheduler.startLoop(Loop4);     //Loop4 calculates injector and ignition timing based on Loop3
+   Scheduler.startLoop(Loop5);     //Loop5 fires the injectors and ignition
  }
 
- void loop() {
-   // put your main code here, to run repeatedly:
+ void loop() {                     //loop runs mics. functions and can handel offloaded tasks from all other loops
+ 
 
  }
 
@@ -28,6 +29,11 @@
  }
 
  void Loop4(){
+
+
+ }
+
+ void Loop5(){
 
 
  }
