215612bc9da20bbbdb508982223b3badf14ee19d
TyMy432 <isaac.smirle@gmail.com>
2018-07-31 08:21:54 -0400

    has errors

--- Arduino Code.cpp
+++ Arduino Code.cpp
@@ -1,18 +1,33 @@
 
 
 
-// void setup() {
-//   // put your setup code here, to run once:
+ void setup() {
+   // put your setup code here, to run once:
   
 
 
 
-//   Scheduler.startLoop(Loop2);     //Loop2 controls running condition sensor inputs and interpretation
-//   Scheduler.startLoop(Loop3);     //Loop3 runs fuel/air/timing algorithems based on Loop2
-//   Scheduler.startLoop(Loop4);     //Loop4 calculates 
-// }
+   Scheduler.startLoop(Loop2);     //Loop2 controls running condition sensor inputs and interpretation
+   Scheduler.startLoop(Loop3);     //Loop3 runs fuel/air/timing algorithems based on Loop2
+   Scheduler.startLoop(Loop4);     //Loop4 calculates 
+ }
 
-// void loop() {
-//   // put your main code here, to run repeatedly:
+ void loop() {
+   // put your main code here, to run repeatedly:
 
-// }
+ }
+
+ void Loop2(){
+
+
+ }
+
+ void Loop3(){
+
+
+ }
+
+ void Loop4(){
+
+
+ }
