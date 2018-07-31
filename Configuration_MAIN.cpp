909440c6c2feb798a7da9874f874f9aed8e755c7
TyMy432 <isaac.smirle@gmail.com>
2018-07-31 16:27:31 -0400

    creating config vars

--- Configuration_MAIN.cpp
+++ Configuration_MAIN.cpp
@@ -8,9 +8,9 @@ int cylNum =    8                          ; //Defines how many cylinders are pr
 int firOrd[] ={ 1, 5, 4, 2, 6, 3, 7, 8   } ; //Defines the firing order
 int bore =      10.16                      ; //Defines cylinder bore (centimeters)
 int stroke =    7.62                       ; //Defines cylender stroke (centimeters)
-int cam[4] ={   0.00, 0.00 , 0.00 , 0.00 } ; //Defines the cam lift and duration format: { <intake lift> , <intake duration> , <exhaust lift> , <exhaust duration> } (milimeters)
+int cam[] ={   0.00, 0.00 , 0.00 , 0.00 }  ; //Defines the cam lift and duration format: { <intake lift> , <intake duration> , <exhaust lift> , <exhaust duration> } (milimeters)
 
-int cylDisp = disp / cylNum ;                  //Auto-calculated value, per-cylinder displacement
+int cylDisp = disp / cylNum ;                //Auto-calculated value, per-cylinder displacement
 
 
 
